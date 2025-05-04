1b8b63cafd46d7b057353e28a0dd9bd7c0ff7ed1
RustamBH <kamaji@mail.ru>
2025-05-05 00:41:35 +0400

    Update client_tests.cpp

--- test/client_tests.cpp
+++ test/client_tests.cpp
@@ -13,12 +13,13 @@ TEST(ClientTest, ConnectToServer) {
         ctx.set_default_verify_paths();
         ctx.set_verify_mode(ssl::verify_none); // verify_peer	
 	
-	EXPECT_NO_THROW({     
+	//EXPECT_NO_THROW({     
 		https_client::HttpsClient client{ ioc, ctx };
 		client.request("localhost", "4433", "/");   
 		ioc.run();
 		std::string response = client.get_response();		
-        EXPECT_FALSE(response.empty());
+        EXPECT_FALSE(response.empty()
+		//);
     });	
 }
 
