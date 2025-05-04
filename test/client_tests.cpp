dfce7338dbc671cca0cd866251174af50ffc5862
RustamBH <kamaji@mail.ru>
2025-05-05 00:25:59 +0400

    Update client_tests.cpp

--- test/client_tests.cpp
+++ test/client_tests.cpp
@@ -7,9 +7,11 @@ TEST(ClientTest, ConnectToServer) {
 	net::io_context ioc;
 	ssl::context ctx{ ssl::context::tlsv12_client };	
 	
-	https_client::HttpsClient client{ ioc, ctx };
+        ctx.set_default_verify_paths();
+        ctx.set_verify_mode(ssl::verify_none); // verify_peer	
 	
 	EXPECT_NO_THROW({     
+		https_client::HttpsClient client{ ioc, ctx };
 		client.request("localhost", "4433", "/");   
 		ioc.run();
 		std::string response = client.get_response();		
@@ -20,10 +22,12 @@ TEST(ClientTest, ConnectToServer) {
 TEST(ClientTest, HandleInvalidPath) {
 	net::io_context ioc;
 	ssl::context ctx{ ssl::context::tlsv12_client };	
-	
-	https_client::HttpsClient client{ ioc, ctx };
+        
+	ctx.set_default_verify_paths();
+        ctx.set_verify_mode(ssl::verify_none); // verify_peer		
 	
 	EXPECT_NO_THROW({     
+		https_client::HttpsClient client{ ioc, ctx };
 		client.request("localhost", "4433", "/nonexistent");   
 		ioc.run();
 		std::string response = client.get_response();		
