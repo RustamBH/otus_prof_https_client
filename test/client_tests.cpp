14dfb5732dde7529186c202e154b1da1eec4c1fa
RustamBH <kamaji@mail.ru>
2025-05-05 00:45:18 +0400

    Update client_tests.cpp

--- test/client_tests.cpp
+++ test/client_tests.cpp
@@ -3,24 +3,18 @@
 
 using namespace https_client;
 
-#define EXPECT_NO_THROW(statement) \
-  GTEST_TEST_NO_THROW_(statement, GTEST_NONFATAL_FAILURE_)
-
 TEST(ClientTest, ConnectToServer) {
 	net::io_context ioc;
 	ssl::context ctx{ ssl::context::tlsv12_client };	
 	
         ctx.set_default_verify_paths();
         ctx.set_verify_mode(ssl::verify_none); // verify_peer	
-	
-	//EXPECT_NO_THROW({     
-		https_client::HttpsClient client{ ioc, ctx };
-		client.request("localhost", "4433", "/");   
-		ioc.run();
-		std::string response = client.get_response();		
-        EXPECT_FALSE(response.empty()
-		//);
-    });	
+
+	https_client::HttpsClient client{ ioc, ctx };
+	client.request("localhost", "4433", "/");   
+	ioc.run();
+	std::string response = client.get_response();		
+        ASSERT_FALSE(response.empty();
 }
 
 TEST(ClientTest, HandleInvalidPath) {
@@ -30,12 +24,10 @@ TEST(ClientTest, HandleInvalidPath) {
 	ctx.set_default_verify_paths();
         ctx.set_verify_mode(ssl::verify_none); // verify_peer		
 	
-	EXPECT_NO_THROW({     
-		https_client::HttpsClient client{ ioc, ctx };
-		client.request("localhost", "4433", "/nonexistent");   
-		ioc.run();
-		std::string response = client.get_response();		
-        EXPECT_TRUE(response.find("404 Not Found") != std::string::npos);
-    });	
-
+	   
+	https_client::HttpsClient client{ ioc, ctx };
+	client.request("localhost", "4433", "/nonexistent");   
+	ioc.run();
+	std::string response = client.get_response();		
+        ASSERT_FALSE(response.find("404 Not Found") != std::string::npos);
 }
